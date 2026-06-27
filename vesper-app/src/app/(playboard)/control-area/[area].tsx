a111b8ba9703907ca7ea3ec2df427877fe41a2cf
lifeopener <jangkangil@gmail.com>
2026-06-27 10:40:49 +0900

    Fix all nested import paths

--- vesper-app/src/app/(playboard)/control-area/[area].tsx
+++ vesper-app/src/app/(playboard)/control-area/[area].tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
-import { PLAYBOARD_DATA } from '../../../../playboard-registry/data';
-import StatusBadge from '../../../../components/playboard/StatusBadge';
+import { PLAYBOARD_DATA } from '../../../playboard-registry/data';
+import StatusBadge from '../../../components/playboard/StatusBadge';
 import { Link } from 'expo-router';
 
 export default function ControlAreaDetail() {
